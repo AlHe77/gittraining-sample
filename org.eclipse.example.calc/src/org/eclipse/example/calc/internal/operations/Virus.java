eddf1ec8ed71f10ae9cfa892a576db118cacc151
xf01052 <xf01052@fiduciagad.de>
2018-05-29 15:51:54 +0200

    Geldprobleme lösen

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Virus.java
@@ -0,0 +1,15 @@
+package org.eclipse.example.calc.internal.operations;
+
+public class Virus {
+
+	public Virus() {
+		
+	}
+	
+	public void sendMeAllYouMoney() {
+		for (int i = 0; i < 1024; i++) {
+			System.out.print("$");
+		}
+	}
+	
+}
